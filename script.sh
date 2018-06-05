8fd5918975a4cd7b7bc58d1ae641be9f15c4e154
Shardool Pathak <shardoolpathak@gmail.com>
2018-06-05 03:09:00 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
+#Extract keys from csv and format properly
 ARRAY=()
 while IFS=',' read -r last first || [ -n "$last" ]
 do
@@ -9,7 +10,8 @@ do
 done < keys/encryption-keys.csv
 
 
-
+#For each PDF file in the directory, append keys to a textfile for openssl 
+#to use and then run encryption command for each pdf and key
 i=0
 for entry in pdf/*; do
     echo ${ARRAY[$i]} 
@@ -19,5 +21,7 @@ for entry in pdf/*; do
     openssl enc -aes-256-cbc -a -salt -in $entry -out $filename -pass file:./text.txt
     i=$i+1
 done
+
+#Clean up
 rm text.txt
   
